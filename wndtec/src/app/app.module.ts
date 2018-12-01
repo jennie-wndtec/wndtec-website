598affbb7398c0ac775b8ba319f475f5db34ddbc
Jennie Håkansson <jennie.hakansson@arbetsformedlingen.se>
2018-12-01 23:05:59 +0100

    - adds header and a star page

--- wndtec/src/app/app.module.ts
+++ wndtec/src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { StartPageComponent } from "./components/start-page/start-page.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, StartPageComponent],
+  imports: [BrowserModule, NgbModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ wndtec/src/app/components/start-page/start-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "wndtec-start-page",
+  templateUrl: "./start-page.component.html",
+  styleUrls: ["./start-page.component.scss"]
+})
+export class StartPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
